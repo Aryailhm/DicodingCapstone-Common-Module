611010edfa8758357fc578161ee22dd9298d542e
Aryailhm <arya.ilham@misteraladin.com>
2023-06-24 11:04:26 +0700

    add assethelper class

---
+++ Common/Core/Utils/AssetHelper.swift
@@ -0,0 +1,15 @@
+//
+//  AssetHelper.swift
+//  Common
+//
+//  Created by Arya Moehammad Ilham on 24/06/23.
+//
+
+import Foundation
+import UIKit
+
+public class AssetHelper {
+    public static func getImage(name: String) -> UIImage? {
+        return UIImage.init(named: "name", in: Bundle(for: self), with: nil)
+    }
+}
